860896231e20fff8cde5beb89552e6c71379a865
Jedrek00 <jedrek.koscielniak@gmail.com>
2021-12-01 07:41:51 +0100

    solution for day one tasks

---
+++ Day01/day_one.py
@@ -0,0 +1,30 @@
+def load_data():
+    with open('input.txt', 'r') as f:
+        data = [int(x) for x in f]
+        return data
+
+
+def task_one(measurements):
+    counter = 0
+    for i in range(1, len(measurements)):
+        if measurements[i] > measurements[i - 1]:
+            counter += 1
+    return counter
+
+
+def task_two(measurements):
+    counter = 0
+    for i in range(0, len(measurements) - 3):
+        if measurements[i] < measurements[i + 3]:
+            counter += 1
+    return counter
+        
+
+def main():
+    data = load_data()
+    print(task_one(data))
+    print(task_two(data))
+
+
+if __name__ == '__main__':
+    main()
